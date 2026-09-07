e42092e1a3e5f2cbe650a26998ac9d119f4403e6
피우리 <piwoorii@gmail.com>
2026-09-07 16:58:31 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/gifttogether/GiftTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.gifttogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gifttogether/GiftTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifttogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
